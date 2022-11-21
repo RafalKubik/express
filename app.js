afc4904896a6d070ea5649b2b3b529982057a40d
RafalKubik <rafik1122@wp.pl>
2022-11-21 23:14:43 +0100

    menu

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+const port = 3000
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,9 +9,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var newsRouter = require('./routes/news');
+var adminRouter = require('./routes/admin');
+var quizRouter = require('./routes/quiz');
 
 var app = express();
+app.listen(port, () => console.log(`Dolphin app listening on port ${port}!`))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,16 +26,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+  res.locals.path = req.path;
+  next();
+});
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/news', newsRouter);
+app.use('/quiz', quizRouter);
+app.use('/admin', adminRouter);
+
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
